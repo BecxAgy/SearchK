d578d70ff9ecd110fbdeb7cc17424a9ea1b155f4
rebeca <beca.cout@gmail.com>
2023-12-13 12:06:32 -0300

    task: implements logic signIn

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,45 @@
+import User from "@/models/user";
+import { connectToDB } from "@/utils/database";
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+
+import React from "react";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  async session({ session }) {
+    //find one by email
+    const userSession = await User.findOne({
+      email: session.user.email,
+    });
+
+    //take id
+    session.user.id = userSession.user._id;
+
+    return session;
+  },
+
+  async signIn({ profile }) {
+    try {
+    } catch (error) {
+      //Connect
+      await connectToDB();
+
+      //check if user already exists
+      const hasUser = User.findOne({
+        email: this.session.user.email,
+      });
+
+      //if not exists, create new user
+      if (!hasUser) {
+      }
+    }
+  },
+});
+
+export { handler as GET, handler as POST };
